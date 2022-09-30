f4d19f78645cfcd901b19fab53eea0917cb2754a
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-09-30 20:44:40 +0100

    Initial Commit

---
+++ GiftApp/GiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftAppApp.swift
+//  GiftApp
+//
+//  Created by Shamsuddin Refaei on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

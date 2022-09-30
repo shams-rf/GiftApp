0a891c80c6709ed94b907efcb2c9889014c04465
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-09-30 21:37:26 +0100

    - Setup project folders

--- GiftApp/GiftAppApp.swift
+++ GiftApp/GiftAppApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct GiftAppApp: App {
+    
+    init() {
+        
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ GiftApp/Views/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  GiftApp
+//
+//  Created by Shamsuddin Refaei on 30/09/2022.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
